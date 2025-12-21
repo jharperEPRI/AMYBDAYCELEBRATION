93c8834e479e4a5c740d1d9d5950dc92a9e5c2e3
jharperEPRI <137301709+jharperEPRI@users.noreply.github.com>
2025-12-20 20:08:08 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,25 @@
 
-const CACHE = 'quest-2026-v1';
+// sw.js
+const CACHE = 'quest-2026-v2'; // <-- bump version
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    .then(() => self.skipWaiting())        // take control ASAP
+  );
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    ).then(() => self.clients.claim())     // control all pages
+  );
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
